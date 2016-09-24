4ffad86d7a371fed887c94c9bb53881094d9ce4b
sandrakawombe <sandrakawombe@gmail.com>
2016-09-24 14:24:41 +0300

    my first commit

---
+++ first.py
@@ -0,0 +1,25 @@
+print("Hello")
+
+
+
+motorcycle = ['hoda','yamaha','suzuki']
+print(motorcycle)
+print(motorcycle[0])
+
+motorcycle[0]='bajaj'
+
+
+print(motorcycle)
+
+motorcycle.append('duata')
+print(motorcycle)
+
+motorcycle.insert(0 , 'duatah')
+print(motorcycle)
+del motorcycle[0]
+print(motorcycle)
+
+ motorcycle=['yamaha' , 'dauta' ]
+ popped_motorcycle=motorcycle.pop()
+  print(popped_motorcycle)
+print(motorcycle)
